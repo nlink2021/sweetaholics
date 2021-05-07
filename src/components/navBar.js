77cc08f11ac008942b7ec0d8e972903da1277537
Ismail <ismail.desouki@gmail.com>
2021-05-07 14:28:44 -0700

    commit

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,39 +1,15 @@
 import { Link } from 'react-router-dom'
-import {useContext} from 'react'
-import {UserContext} from '../context/userContext'
 
-const NavBar = (props) =>{
-    const {userState} = useContext(UserContext)
-    const [user,setUser] = userState
-    return(
-        <div className = 'page-header'>
-            <div className='logo'>
-            Sweet-A-Holics 
-            </div>
-
-            {!user.id ?
-            <nav className = 'logged-out-links'>
-                <Link to= '/'>Home</Link>{'|'}
-                <span onClick={()=>{props.setSignupOrLogin('login')}}>
-                    <Link to= '/signup-login'>Login</Link>
-                </span>{'|'}
-                <span onClick={()=>{props.setSignupOrLogin('signup')}}>
-                    <Link to= '/signup-login'>Sign Up</Link>
-                </span>
-            </nav>
-            :
-            <nav className = 'logged-in-links'>
-                <Link to= '/'>Home</Link>{'|'}
-                <Link to= '/products'>All Products</Link>{'|'}
-                <Link to= '/orders'>My Orders</Link>{'|'}
-                <Link to= '/cart'>Cart</Link>{'|'}
-                <span onClick={()=>{
-                    localStorage.removeItem('userId')
-                    setUser([])
-                }}><Link to= '/'>Logout</Link></span>
-            </nav>
-            }
-        </div>
-    )}
+const NavBar = () => {
+    return (
+        <nav className="navFont">
+            <Link to="/">Home</Link>
+            {'  |  '}
+            <Link to="/login">Login</Link>
+            {'  |  '} 
+            <Link to="/signup">Signup</Link>
+        </nav>
+    )
+}
 
 export default NavBar
